059060602911ee82bfa8a96715773f5f9e7e35ac
seaweedbrain <nathanstchepinsky@gmail.com>
2023-08-22 17:42:20 +0200

    Initial Commit

---
+++ Zero-TOTP/Zero_TOTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zero_TOTPApp.swift
+//  Zero-TOTP
+//
+//  Created by Stchepinsky Nathan on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Zero_TOTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
