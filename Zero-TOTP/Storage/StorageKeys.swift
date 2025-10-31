b3720fdc19ba010e585f20f40f51cbbe58f68f1b
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2025-10-30 22:38:09 -0400

    Add DS_store to gitignore

---
+++ Zero-TOTP/Storage/StorageKeys.swift
@@ -0,0 +1,19 @@
+//
+//  UserDefault.swift
+//  Zero-TOTP
+//
+//  Created by Nathan Stchepinsky on 2025-07-20.
+//
+// Manager User settings and information storage
+
+
+import Foundation
+
+
+struct UserDefaultsKeys {
+    static let email = "user_email"
+}
+
+struct TenantDefaultsKeys {
+    static let base_url = "zero_totp_base_url"
+}
